af540c3b6a4c4d3232f4c6e803d3fe9c8324aa85
Diego sastoque <diegoelcairo@gmai.com>
2018-01-12 13:54:27 -0500

    Begining the profile

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
+var $win = $(window);
+
+$win.on('scroll', function() {
+ 	console.log('scroll');
+ })
+
 $(document).ready(function() {
+	var $sun = $('.animated');
+	
+
+
+ 
+
+
+
+
+
 	$('#toggle').click(function(){
 		$('#toggle').toggleClass('on');
 		$('.menu').toggleClass('active');
 	})
+
+
+
 })
