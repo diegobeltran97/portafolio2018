e375bd9749e35a2c43dfac285c6bf3d798fa98d5
Diego sastoque <diegoelcairo@gmai.com>
2018-01-25 18:13:59 -0500

    js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var $win = $(window);
 
+
 $win.on('scroll', function() {
  	console.log('scroll');
  })
@@ -7,11 +8,17 @@ $win.on('scroll', function() {
 
 
 $(document).ready(function() {
+   var selecion = $('.selecion');
+
 	var $sun = $('.animated');
 	$('#toggle').click(function(){
 		$('#toggle').toggleClass('on');
 		$('.menu').toggleClass('active');
 	})
+
+    selecion.click(function(){
+    	$('.menu').toggleClass('active');
+    })
 
 
 
