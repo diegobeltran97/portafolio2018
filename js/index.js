5275f2cb32851472cfcd2ec37309df2ca4c39aba
Diego sastoque <diegoelcairo@gmai.com>
2018-01-25 17:42:43 -0500

    portafolio terminado

--- js/index.js
+++ js/index.js
@@ -4,17 +4,10 @@ $win.on('scroll', function() {
  	console.log('scroll');
  })
 
+
+
 $(document).ready(function() {
 	var $sun = $('.animated');
-	
-
-
- 
-
-
-
-
-
 	$('#toggle').click(function(){
 		$('#toggle').toggleClass('on');
 		$('.menu').toggleClass('active');
